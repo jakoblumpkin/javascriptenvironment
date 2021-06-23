cce8538b9545f622b661d0a8f13d7444197e6815
jakob <jakob.lumpkin@ibm.com>
2021-06-23 18:27:18 -0500

    Fix broken test

--- src/index.test.js
+++ src/index.test.js
@@ -9,14 +9,14 @@ describe('Our first test', () => {
   });
 });
 
-describe("index.html", () => {
-  it("should say hello", () => {
-    const index = fs.readFileSync("./src/index.html", "utf-8");
-    const { JSDOM } = jsdom;
-    console.log(JSDOM);
-    const dom = new JSDOM(index);
-    const h1 = dom.window.document.getElementsByTagName("h1")[0];
-    expect(h1.innerHTML).to.equal("Hello World!");
-    dom.window.close();
-  });
-});
+describe('index.html', () => {
+  it('should have h1 that says Users', (done) => {
+    const index = fs.readFileSync('./src/index.html', "utf-8");
+    jsdom.env(index, function(err, window) {
+      const h1 = window.document.getElementsByTagName('h1')[0];
+      expect(h1.innerHTML).to.equal("Hello World!");
+      done();
+      window.close();
+    });
+  })
+})
